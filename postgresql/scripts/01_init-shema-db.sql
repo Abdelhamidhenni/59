b813851f98059a189c122569b265890df3b5591c
Abdelhamid <henni.abdelhamid@gmail.com>
2020-11-04 15:28:45 +0100

    fix(nt): init db

--- postgresql/scripts/01_init-shema-db.sql
+++ postgresql/scripts/01_init-shema-db.sql
@@ -1,8 +1,8 @@
-CREATE SCHEMA IF NOT EXISTS '59';
+CREATE SCHEMA IF NOT EXISTS 59;
 
 SHOW search_path;
 
-ALTER ROLE '59' RESET search_path;
+ALTER ROLE 59 RESET search_path;
 
 -- SET search_path TO 59;
 SHOW search_path;
