491e3b6b3ef039f97cd6ccc65d27dc4713a69f81
malouloumz <malou.anglade@gmail.com>
2020-11-04 18:33:05 +0100

    wip: select

--- client/src/components/IndicatorsGlobal.js
+++ client/src/components/IndicatorsGlobal.js
@@ -2,34 +2,33 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
-const indicatorsGlobal = [
-  {
-    title: "Ville - Code postal",
-    score: "94350"
-  },
-  {
-    title: "Population",
-    score: "50 000"
-  },
-  {
-    title: "Score global",
-    score: "80"
-  }
-];
-
-export default function IndicatorsGlobal() {
+export default function IndicatorsGlobal({ city, globalScore }) {
   return (
     <Grid container spacing={10} alignItems="center">
-      {indicatorsGlobal.map(indicator => (
-        <Grid key={indicator.title} item xs={12} sm={4} md={4}>
-          <Typography component="h2" variant="h6" color="textPrimary">
-            {indicator.title}
-          </Typography>
-          <Typography component="p" variant="h4" color="textSecondary">
-            {indicator.score}
-          </Typography>
-        </Grid>
-      ))}
+      <Grid item xs={12} sm={4} md={4}>
+        <Typography component="h2" variant="h6" color="textPrimary">
+          Ville - Code postal
+        </Typography>
+        <Typography component="p" variant="h4" color="textSecondary">
+          {city.name}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={4} md={4}>
+        <Typography component="h2" variant="h6" color="textPrimary">
+          Population
+        </Typography>
+        <Typography component="p" variant="h4" color="textSecondary">
+          {city.population}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={4} md={4}>
+        <Typography component="h2" variant="h6" color="textPrimary">
+          Score global
+        </Typography>
+        <Typography component="p" variant="h4" color="textSecondary">
+          {globalScore}
+        </Typography>
+      </Grid>
     </Grid>
   );
 }
