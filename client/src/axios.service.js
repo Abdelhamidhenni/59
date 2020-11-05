97bd5fc1952afdba72b569ddc9bab409ca9b1cb1
malouloumz <malou.anglade@gmail.com>
2020-11-05 11:23:26 +0100

    fix: add baseName

--- client/src/axios.service.js
+++ client/src/axios.service.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export const fetchMunicipalities = async () => {
   try {
-    const municipalities = await axios.get(
-      `http://localhost/api/municipalities`
-    );
+    const municipalities = await axios.get(`/api/municipalities`, {
+      baseURL: window.location.origin
+    });
     return municipalities.data;
   } catch (error) {
     throw Error("Error during getting municipalities");
