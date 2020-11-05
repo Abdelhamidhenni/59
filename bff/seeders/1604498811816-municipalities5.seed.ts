6bddd092876055504b8d809b1ec09e8f2a886b1e
zes <cyril.gerla@protonmail.com>
2020-11-05 09:33:15 +0100

    feat: seeds municipalities competentences data

---
+++ bff/seeders/1604498811816-municipalities5.seed.ts
@@ -0,0 +1,10 @@
+import { Factory, Seeder } from 'typeorm-seeding';
+import { Connection } from 'typeorm';
+
+import { Municipality } from '../src/modules/municipality/municipality.entity';
+import * as municipalities from './datas/municipalities-5.json';
+
+export default class CreateMunicipality implements Seeder {
+  run = async (_factory: Factory, connection: Connection): Promise<any> =>
+    await connection.createQueryBuilder().insert().into(Municipality).values(municipalities).execute();
+}
